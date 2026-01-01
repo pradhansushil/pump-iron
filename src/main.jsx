73d34bfb531e7a61245095abc2dc6b9c9ad4cbbc
Sushil Pradhan <sushil.frontenddev@gmail.com>
2026-01-01 20:30:20 +0545

    Wrap App with BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Creates the root React element and attaches it to the 'root' div in index.html
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // Development tool that activates additional checks and warnings to catch bugs
+  <React.StrictMode>
+    {/* Enables routing functionality throughout the entire app */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
