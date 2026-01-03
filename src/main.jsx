b58f4b58a337660e5b3cfadd1e9e1173b6e0ff4e
Sushil Pradhan <sushil.frontenddev@gmail.com>
2026-01-03 08:14:34 +0545

    Wrap App with AuthProvider to enable auth context

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import { AuthProvider } from "./contexts/AuthContext.jsx";
 
 // Creates the root React element and attaches it to the 'root' div in index.html
 ReactDOM.createRoot(document.getElementById("root")).render(
   // Development tool that activates additional checks and warnings to catch bugs
   <React.StrictMode>
-    {/* Enables routing functionality throughout the entire app */}
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    {/* Provides authentication state and functions to all components in the app */}
+    <AuthProvider>
+      {/* Enables routing functionality throughout the entire app */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
 );
