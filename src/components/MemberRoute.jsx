5c4421b07a98d6b57e7003e6ada13a79359b2365
Sushil Pradhan <sushil.frontenddev@gmail.com>
2026-01-11 20:06:52 +0545

    Modify: Fix spelling for contexts to context

--- src/components/MemberRoute.jsx
+++ src/components/MemberRoute.jsx
@@ -1,5 +1,5 @@
 import { Navigate } from "react-router-dom";
-import { useAuth } from "../contexts/AuthContext";
+import { useAuth } from "../context/AuthContext";
 import ProtectedRoute from "./ProtectedRoute";
 
 export default function MemberRoute({ children }) {
--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,5 +1,5 @@
 import { Navigate } from "react-router-dom";
-import { useAuth } from "../contexts/AuthContext";
+import { useAuth } from "../context/AuthContext";
 
 export default function ProtectedRoute({ children }) {
   const { currentUser, loading } = useAuth();
