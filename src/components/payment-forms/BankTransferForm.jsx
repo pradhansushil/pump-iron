eb5131038954a43dabf6e4d1bf0ae99fb43e5167
Sushil Pradhan <sushil.frontenddev@gmail.com>
2026-02-16 04:52:08 +0545

    Add account and routing number inputs

---
+++ src/components/payment-forms/BankTransferForm.jsx
@@ -0,0 +1,26 @@
+export default function BankTransferForm({
+  accountNumber,
+  setAccountNumber,
+  routingNumber,
+  setRoutingNumber,
+}) {
+  return (
+    <div>
+      <label htmlFor="accountNumber">Account Number</label>
+      <input
+        id="accountNumber"
+        type="text"
+        onChange={(e) => setAccountNumber(e.target.value)}
+        value={accountNumber}
+      />
+
+      <label htmlFor="routingNumber">Routing Number</label>
+      <input
+        id="routingNumber"
+        type="text"
+        onChange={(e) => setRoutingNumber(e.target.value)}
+        value={routingNumber}
+      />
+    </div>
+  );
+}
