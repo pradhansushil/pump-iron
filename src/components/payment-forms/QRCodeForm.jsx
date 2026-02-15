93d75191e0ec556d4017d7e2bc0d1ed586fb9fb3
Sushil Pradhan <sushil.frontenddev@gmail.com>
2026-02-16 04:53:11 +0545

    Add QR Code payment display

---
+++ src/components/payment-forms/QRCodeForm.jsx
@@ -0,0 +1,8 @@
+export default function QRCodeForm() {
+  return (
+    <div>
+      <p>Scan this code with your payment app (Cash App, Zelle, etc.)</p>
+      {/* <img src="..." alt="QR Code for payment" /> */}
+    </div>
+  )
+};
