9a3461f3fd46d0f6b672c35bfa83b45eb0fbcf79
Sushil Pradhan <sushil.frontenddev@gmail.com>
2026-02-28 04:51:15 +0545

    feat: add Features section with semantic structure and accessibility

--- src/components/home/features/Features.jsx
+++ src/components/home/features/Features.jsx
@@ -3,15 +3,18 @@ import FeatureCard from "./FeatureCard";
 
 export default function Features() {
   return (
-    <ul>
-      {featuresData.map((feature, index) => (
-        <FeatureCard
-          key={index}
-          icon={feature.icon}
-          title={feature.title}
-          description={feature.description}
-        />
-      ))}
-    </ul>
+    <section aria-labelledby="features">
+      <h2 id="features">Why Join Us?</h2>
+      <ul>
+        {featuresData.map((feature, index) => (
+          <FeatureCard
+            key={index}
+            icon={feature.icon}
+            title={feature.title}
+            description={feature.description}
+          />
+        ))}
+      </ul>
+    </section>
   );
 }
