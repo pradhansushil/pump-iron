2de54475ad18f20a17ce2a8969b13b24251a73a1
Sushil Pradhan <sushil.frontenddev@gmail.com>
2026-02-27 07:02:56 +0545

    feat: add static features data for Features section

---
+++ src/data/featuresData.js
@@ -0,0 +1,19 @@
+export const featuresData = [
+  {
+    icon: "classes-icon",
+    title: "Classes",
+    description:
+      "From HIIT to yoga, we offer a wide range of classes for all fitness levels.",
+  },
+  {
+    icon: "gym-equipment-icon",
+    title: "Equipment",
+    description: "State of the art equipment maintained and updated regularly.",
+  },
+  {
+    icon: "gym-trainer-icon",
+    title: "Trainers",
+    description:
+      "Certified trainers dedicated to helping you reach your fitness goals.",
+  },
+];
