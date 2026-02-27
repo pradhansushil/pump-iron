6e3cac62a94a28c133aeb9ec4d8c5a993d63ea65
Sushil Pradhan <sushil.frontenddev@gmail.com>
2026-02-28 04:52:43 +0545

    feat: add hero and features sections to Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Hero from "../components/home/Hero";
+import Features from "../components/home/features/Features";
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      <Features />
+    </main>
+  );
+}
