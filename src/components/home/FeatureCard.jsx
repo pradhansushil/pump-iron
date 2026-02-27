ccf961f132a323ee3f759322fc9fe60e9226dccd
Sushil Pradhan <sushil.frontenddev@gmail.com>
2026-02-27 07:42:20 +0545

    fix: rename UpdatePaymetMethodModal to UpdatePaymentMethodModal

--- src/components/home/FeatureCard.jsx
+++
@@ -1,9 +0,0 @@
-export default function FeatureCard({ icon, title, description }) {
-  return (
-    <li>
-      <span>{icon}</span>
-      <h2>{title}</h2>
-      <p>{description}</p>
-    </li>
-  );
-}
