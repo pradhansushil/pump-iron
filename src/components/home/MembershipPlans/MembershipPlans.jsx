7800c224a5c16b80f380b48f1ba7015c08d8b0d9
Sushil Pradhan <sushil.frontenddev@gmail.com>
2026-02-28 07:52:40 +0545

    fix: correct mismatched export name in plansData import

--- src/components/home/MembershipPlans/MembershipPlans.jsx
+++ src/components/home/MembershipPlans/MembershipPlans.jsx
@@ -1,4 +1,4 @@
-import { plansData } from "../../../data/plansData";
+import { plans } from "../../../data/plansData";
 import MembershipPlanCard from "./MembershipPlanCard";
 
 export default function MembershipPlans() {
@@ -6,7 +6,7 @@ export default function MembershipPlans() {
     <section aria-labelledby="membership-plans">
       <h2 id="membership-plans">Plans</h2>
       <ul>
-        {plansData.map((plan) => (
+        {plans.map((plan) => (
           <MembershipPlanCard
             key={plan.name}
             name={plan.name}
