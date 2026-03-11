4622dfb9d2c67240335ad7a844509cbd0768a818
Sushil Pradhan <sushil.frontenddev@gmail.com>
2026-03-12 05:25:18 +0545

    refactor: move AdminRoute to correct directory and update import

---
+++ src/components/routes/Route.jsx
@@ -0,0 +1,13 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
+import ProtectedRoute from "./Protected";
+
+export default function AdminRoute({ children }) {
+  const { userRole } = useAuth();
+
+  return (
+    <ProtectedRoute>
+      {userRole !== "admin" ? <Navigate to="/" replace /> : children}
+    </ProtectedRoute>
+  );
+}
