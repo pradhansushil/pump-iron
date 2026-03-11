a8920cd9721368a47dff7e9b3b8039a40efabc26
Sushil Pradhan <sushil.frontenddev@gmail.com>
2026-03-12 05:24:21 +0545

    refactor: move AdminRoute to correct directory and update import

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import Gallery from "./pages/public/Gallery";
 
 // Routes
 import MemberRoute from "./components/routes/Member";
-import AdminRoute from "./components/admin/Route";
+import AdminRoute from "./components/routes/Route";
 import PublicRoute from "./components/routes/Public";
 
 // Member pages
