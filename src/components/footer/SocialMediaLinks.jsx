74c6c7751f55cde2634737ed232f8028b10c1e80
Sushil Pradhan <sushil.frontenddev@gmail.com>
2026-03-12 05:52:09 +0545

    fix: replace placeholder social links with real GitHub profile link

--- src/components/footer/SocialMediaLinks.jsx
+++ src/components/footer/SocialMediaLinks.jsx
@@ -2,28 +2,7 @@ export default function SocialMediaLinks() {
   return (
     <section aria-labelledby="social-media-links">
       <h3 id="social-media-links">Social Media</h3>
-      <ul>
-        <li>
-          <a href="#" target="_blank" rel="noreferrer">
-            Facebook
-          </a>
-        </li>
-        <li>
-          <a href="#" target="_blank" rel="noreferrer">
-            Instagram
-          </a>
-        </li>
-        <li>
-          <a href="#" target="_blank" rel="noreferrer">
-            Twitter
-          </a>
-        </li>
-        <li>
-          <a href="#" target="_blank" rel="noreferrer">
-            Youtube
-          </a>
-        </li>
-      </ul>
+      <a href="https://github.com/pradhansushil">GitHub</a>
     </section>
   );
 }
