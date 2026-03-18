8e13ff64306f219b8b1ac7a91d8070a1ffc4b466
Sushil Pradhan <sushil.frontenddev@gmail.com>
2026-03-18 07:45:47 +0545

    feat: install and configure Tailwind CSS

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+
+import "./index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
 
@@ -15,5 +17,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <App />
       </BrowserRouter>
     </AuthProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
