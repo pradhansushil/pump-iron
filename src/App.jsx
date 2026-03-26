1c413959c22df3b82f631d2c959f544f33b37f39
Sushil Pradhan <sushil.frontenddev@gmail.com>
2026-03-26 06:11:36 +0545

    chore: remove redundant import comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Routes, Route, useLocation } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
-// Public-facing pages
 import Home from "./pages/public/Home";
 import Footer from "./components/footer/Footer";
 import LoginPage from "./pages/LoginPage";
@@ -10,18 +9,15 @@ import Navbar from "./components/Navbar";
 import Employees from "./pages/public/Employees";
 import Gallery from "./pages/public/Gallery";
 
-// Routes
 import MemberRoute from "./components/routes/Member";
 import AdminRoute from "./components/routes/Admin";
 import PublicRoute from "./components/routes/Public";
 
-// Member pages
 import Payments from "./pages/Payments";
 import MemberDashboard from "./pages/MemberDashboard";
 import Classes from "./pages/Classes";
 import Profile from "./pages/Profile";
 
-// Admin pages
 import AdminDashboard from "./pages/admin/Dashboard";
 import AdminMembers from "./pages/admin/Members";
 import AdminLayout from "./components/admin/Layout";
@@ -35,16 +31,14 @@ export default function App() {
   const isAdminRoute = location.pathname.startsWith("/admin");
   const isAuthRoute = ["/login", "/signup"].includes(location.pathname);
 
-  const appStyle = !isAdminRoute ? { paddingTop: "64px" } : null;
+  const appStyle =
+    !isAdminRoute && !isAuthRoute ? { paddingTop: "64px" } : null;
 
   return (
     <div className="flex flex-col min-h-screen" style={appStyle}>
-      {/* Navbar component handles all navigation logic */}
       {!isAdminRoute && !isAuthRoute && <Navbar />}
       <Toaster position="top-right" />
-
-      {/* Routes define which component renders for each URL path */}
-      <div className="flex-1 pb-16 ">
+      <div className={`flex-1 ${!isAdminRoute && !isAuthRoute ? "pb-16" : ""}`}>
         <Routes>
           <Route
             path="/"
@@ -59,7 +53,6 @@ export default function App() {
           <Route path="/classes" element={<Classes />} />
           <Route path="/employees" element={<Employees />} />
           <Route path="/gallery" element={<Gallery />} />
-
           <Route
             path="/dashboard"
             element={
